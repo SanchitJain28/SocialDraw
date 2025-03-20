ebb095df5b3c665c69abbc2146f7f07db9d8d90c
SanchitJain28 <mythichuman28@gmail.com>
2025-03-20 16:21:12 +0530

    updates

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,41 +1,57 @@
 import React, { useEffect, useState } from 'react'
-import {  h2, h3, primaryButton, Primarypara } from '../Themeclasses'
+import { h2, h3, primaryButton, Primarypara } from '../Themeclasses'
 import DrawingCard from './DrawingCard'
 import { Axios } from '../ApiFormat'
+import { useNavigate } from 'react-router'
+import { socket } from '../Socket'
 
 export default function Dashboard() {
-    const [drawings,setDrawings]=useState<any>(null)
-    const [loading,setLoading]=useState<boolean>(true)
-    const Fetchdrawings=async()=>{
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    const navigate = useNavigate()
+    const [drawings, setDrawings] = useState<any>(null)
+    const [loading, setLoading] = useState<boolean>(true)
+    const Fetchdrawings = async () => {
         setLoading(true)
         try {
-            const response=await Axios.get("/api/drawings")
+            const response = await Axios.get("/api/drawings")
             console.log(response)
             setDrawings(response.data.data)
         } catch (error) {
             console.log(error)
         }
-        finally{
+        finally {
             setLoading(false)
         }
     }
     useEffect(() => {
-      Fetchdrawings()
+        function onConnect() {
+            setIsConnected(true);
+            console.log("Connected")
+        }
+
+        function onDisconnect() {
+            setIsConnected(false);
+        }
+        socket.on('connect', onConnect);
+        socket.on('disconnect', onDisconnect);
+        Fetchdrawings()
     }, [])
-    const [create,setCreate]=useState<boolean>(false)
-    if(loading){
+    const [create, setCreate] = useState<boolean>(false)
+    if (loading) {
         return <p className={h3}>Loading...</p>
     }
     return (
         <div className='p-8'>
             <p className={h2 + "my-4"}>Create a Drawing</p>
-            <button className={primaryButton} onClick={()=>{
+            <button className={primaryButton} onClick={() => {
                 setCreate(true)
             }}>Create</button>
-            {create && <DrawingCard/>}
+            {create && <DrawingCard />}
             <div className="grid grid-cols-3">
-                {drawings && drawings.map((e,index:number)=>{
-                    return <div key={index} className="border border-zinc-600 rounded-xl p-4 my-4 mr-4">
+                {drawings && drawings.map((e, index: number) => {
+                    return <div key={index} onClick={() => {
+                        navigate(`/draw?id=${e._id}`)
+                    }} className="border border-zinc-600 rounded-xl p-4 my-4 mr-4">
                         <p className={Primarypara}>{e.title}</p>
                     </div>
                 })}
--- src/components/Draw.tsx
+++ src/components/Draw.tsx
@@ -1,49 +1,101 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Excalidraw } from "@excalidraw/excalidraw";
 import "@excalidraw/excalidraw/index.css";
-import { h4, WarningButton } from '../Themeclasses';
+import { h2, primaryButton, Primarypara, Secondarypara } from '../Themeclasses';
 import { useDebounce } from "@uidotdev/usehooks";
 import { ToastContainer, toast } from 'react-toastify';
-import { useSearchParams } from 'react-router';
+import { useNavigate, useSearchParams } from 'react-router';
 import { Axios } from '../ApiFormat';
 
 
-export default function Draw() {
-  let [searchParams] = useSearchParams();
 
-  const [excalidrawAPI, setExcalidrawAPI] = useState<any>(null)
+export default function Draw() {
+  const navigate=useNavigate()
+  const [saving,setSaving]=useState<boolean>(false)
+  const [searchParams] = useSearchParams();
+  const [initialDrawings, setInitialDrawings] = useState<any>(null)
+  const [drawingData,setDrawingData]=useState<any>(null)
+  // const [excalidrawAPI, setExcalidrawAPI] = useState<any>(null)
   const [sceneElements, setSceneElements] = useState<any>()
   const debouncedSceneElements = useDebounce(sceneElements, 1000);
   const notify = () => toast("Saving");
-
+  const deleteDrawing = async () => {
+    try {
+      const response = await Axios.delete(`/api/delete-drawing?id=${searchParams.get("id")}`)
+      console.log(response)
+      navigate("/dashboard")
+      toast("Drawing deleted successfully")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const getDrawing = async () => {
+    try {
+      const response = await Axios.get(`/api/single-drawing?id=${searchParams.get("id")}`)
+      const { data } = response.data
+      console.log(data)
+      setDrawingData(data[0])
+      setInitialDrawings(data[0].elements)
+      setSceneElements(data[0].elements)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   const handleOnchange = (excalidrawElements) => {
     setSceneElements(excalidrawElements)
   }
   useEffect(() => {
-   console.log(searchParams.get("id"))
+    getDrawing() // get initial drawing data from API when component mounts
+    // console.log(searchParams.get("id")) // print the id from the URL parameter
+    console.log(searchParams.get("id"))
   }, [])
-  useEffect(()=>{
-    const updateDrawing=async()=>{
-      if (debouncedSceneElements) {
+  useEffect(() => {
+    const updateDrawing = async () => {
+      if (debouncedSceneElements && initialDrawings) {
+        setSaving(true)
         // setSceneElements(excalidrawAPI.getSceneElements())
-        const response=await Axios.post(`/api/update-drawing?id=${searchParams.get("id")}`,{
-          drawings:sceneElements
-        })
-        console.log(response)
-        notify()
-        console.log(sceneElements)
+        try {
+          const response = await Axios.post(`/api/update-drawing?id=${searchParams.get("id")}`, {
+            drawings: sceneElements
+          })
+          console.log(response)
+          notify()
+          console.log(sceneElements)
+        } catch (error) {
+          console.log(error)
+        }
+        finally {
+          setSaving(false)
+        }
       }
     }
     updateDrawing()
-  },[debouncedSceneElements])
+  }, [debouncedSceneElements])
+  if(!initialDrawings){
+    return <div>
+      <p className={h2}>Loading...</p>
+    </div>
+  }
   return (
-    <>
-      <div style={{ height: "100vh", borderRadius: "0px" }} className='custom-styles rounded-full '>
-        <p className={h4 +"absolute top-0 "} >Hi</p>
-        <Excalidraw theme='dark' excalidrawAPI={(api) => setExcalidrawAPI(api)} onChange={handleOnchange} />
+    <div className='bg-[#121212]'>
+      <div className="border-b border-zinc-600 flex items-center px-8 justify-between">
+        <p className={Primarypara}>{drawingData.title}</p>
+        <p className={Secondarypara}>{saving?"Saving":""}</p>
+        <button className={primaryButton} onClick={deleteDrawing}>delete drawing</button>
       </div>
-      <ToastContainer autoClose={500}/>
-      <button className={WarningButton} onClick={() => console.log(excalidrawAPI.getSceneElements())}>Change background to white</button>
-    </>
+      <div style={{ height: "100vh", borderRadius: "0px" }} className='custom-styles rounded-full '>
+        {initialDrawings && <Excalidraw
+          theme='dark'
+          initialData={{
+            elements: [
+              ...initialDrawings
+            ]
+          }}
+          // excalidrawAPI={(api) => setExcalidrawAPI(api)}
+          onChange={handleOnchange} />}
+      </div>
+      <ToastContainer
+        autoClose={500} />
+    </div>
   )
 }
